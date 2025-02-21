5cbdf09dab7c6fe357b77925aa518237e434a7d7
chiagbanweghi-peter <chiagbanweghimosespeter@gmail.com>
2025-02-21 13:16:35 +0200

    update all script

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""
-get top 10 hot posts function
-"""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
 
-import json
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a subreddit."""
-    if len(sys.argv) < 2:
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
-    else:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        if result.status_code != 200:
-            print(None)
-        else:
-            data = json.loads(result.text)["data"]["children"]
-            for post in data[:10]:
-                print(post["data"]["title"])
